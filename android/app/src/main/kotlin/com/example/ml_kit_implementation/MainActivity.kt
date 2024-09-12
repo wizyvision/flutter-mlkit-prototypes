21549204c8abefac9b9ec9fc3bd0218ac6f62a3d
simonvque <simon.que@benilde.edu.ph>
2024-09-12 13:48:53 +0800

    first push

---
+++ android/app/src/main/kotlin/com/example/ml_kit_implementation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ml_kit_implementation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Document Scanner',
+      /* home: CameraScreen(), */
+    );
+  }
+}
